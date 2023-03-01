0526ed72b01e7a7a9e7b9448f87a2e0f0c034a4e
arellano-gustavo <arellano.gustavo@gmail.com>
2023-03-01 10:49:17 -0600

    Separando la lógica de negocio del controlador REST

--- src/main/java/com/example/helloworld/controller/PersonaController.java
+++ src/main/java/com/example/helloworld/controller/PersonaController.java
@@ -1,11 +1,20 @@
 package com.example.helloworld.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/gus/tavo/arellano")
 public class PersonaController {
-
+    private PersonaService ps = new PersonaService();
     /*
     aqui usaremos por CONVENCION:
         GetMapping    // se usa para recuperar información del server
@@ -20,10 +29,26 @@ public class PersonaController {
             Los códigos 5xx son asociados a acciones de tipo "error del server, no tuyo"
     */
     @GetMapping(value= "/pregunta", produces = "application/json; charset=utf-8")
-    public Persona sendGreetings() {
-        Persona p = new Persona(1, "gus1", "arellano", "sandoval", 123.32F, true);
-        return p;
+    public Persona sendGreetingsGET() {
+        //Persona p = new Persona(1, "gus1GET", "arellano", "sandoval", 123.32F, true);
+//        Persona q = tavito();
+//        return q;
+        return tavito();
     }
+//    @PutMapping(value= "/pregunta", produces = "application/json; charset=utf-8")
+//    public Persona sendGreetingPUT() {
+//        Persona p = new Persona(1, "gus1PUT", "arellano", "sandoval", 123.32F, true);
+//        return p;
+//    }    
+    @PostMapping(value= "/pregunta", produces = "application/json; charset=utf-8")
+    public Persona tavito() {
+        return ps.calcula("algo", "pedregal", "gus1DELETE", "arellanbo", "sandoval");
+    }
+    
+    @DeleteMapping(value= "/pregunta", produces = "application/json; charset=utf-8")
+    public Persona sendGreetingDelete() {
+        return ps.calcula("algo", "pedregal", "gus1DELETE", "arellanbo", "sandoval");
+    } 
     /*
     
     Hay dos formas de pasar parámetros a los endpoint's antoados con GET:
@@ -38,19 +63,58 @@ public class PersonaController {
     Sirvem igual, pero a veces, es mas últil (seménticamente) usar uno de ellos 
     en particular, que sería el que mas se adapta al objeto de la invocación.
     */
-    @GetMapping(
-            value= "/pregunta2", produces = "application/text; charset=utf-8")
-    public String sendGreetings2() {
-        int a =0;
-        System.out.println(1/a);
-        return "{'gus':'tavo','edad':21}".replace('\'', '\"');
+    @PostMapping(
+            value= "/pregunta2", 
+            produces = "application/json; charset=utf-8")
+    public String sendGreetings2(
+            @RequestHeader String apellido,
+            @RequestParam String nombre) {
+        //int a=0;
+        //System.out.println(1/a);
+        return "{'gus':'"+nombre+apellido+"','edad':21}".replace('\'', '\"');
+    }
+    
+    @PostMapping(
+            value= "/guardaPersona", 
+            produces = "application/json; charset=utf-8")
+    public Persona sendGreetings3(@RequestBody Persona p) {
+        System.out.println(p.getNombre());
+        return p;
     }
     
     @GetMapping(
-            value= "/pregunta3", 
+            value= "/test-diagonales/{variable1}/{variable2}/{variable3}", 
             produces = "application/json; charset=utf-8")
-    public Persona sendGreetings3() {
-        Persona p = new Persona(3, "gus3", "arellano", "sandoval", 123.32F, true);
-        return p;
+    public Persona testDiagonalesA(
+            @RequestHeader("User-Agent") String ua,
+            @RequestHeader String colonia,
+            @PathVariable String variable1, 
+            @PathVariable String variable2, 
+            @PathVariable String variable3) {
+        return ps.calcula(ua, colonia, variable1, variable2, variable3);
     }
+    
+    @GetMapping(
+            value= "/test-diagonales/{variable1}/{variable2}", 
+            produces = "application/json; charset=utf-8")
+    public Persona testDiagonalesB(
+            @RequestHeader("User-Agent") String ua,
+            @RequestHeader String colonia,
+            @PathVariable String variable1, 
+            @PathVariable String variable2) {
+        return ps.calcula(ua, colonia, variable1, variable2, "variable3");
+    }
+    
+    @GetMapping(
+            value= "/test-params", 
+            produces = "application/json; charset=utf-8")
+    public Persona testParamsA(
+            @RequestHeader("User-Agent") String ua,
+            @RequestHeader String colonia,
+            @RequestParam String variable1, 
+            @RequestParam String variable2, 
+            @RequestParam String variable3) {
+        return ps.calcula(ua, colonia, variable1, variable2, variable3);
+    }
+
 }
---
+++ src/main/java/com/example/helloworld/controller/PersonaService.java
@@ -0,0 +1,13 @@
+package com.example.helloworld.controller;
+
+public class PersonaService {
+    public Persona calcula(String ua, String col, String v1, String v2, String v3) {
+        Persona p = null;
+        if(ua.toLowerCase().contains("postman")) {
+            p = new Persona(1, v1+col, ua, v3, 888.32F, true);
+        } else {
+            p = new Persona(1, v1+col, ua, v3, 888.32F, true);
+        }
+        return p;
+    }
+}
