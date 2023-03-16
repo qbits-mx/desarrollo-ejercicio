85f59b1c825a696d9875f6801dbfb7f972b461f7
arellano-gustavo <arellano.gustavo@gmail.com>
2023-03-16 01:10:42 -0600

    adding product REST API

--- src/main/resources/local-database/scripts/db.sql
+++ src/main/resources/local-database/scripts/db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.19  Distrib 10.3.29-MariaDB, for debian-linux-gnu (x86_64)
+-- MariaDB dump 10.18  Distrib 10.5.7-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: publisher
 -- ------------------------------------------------------
--- Server version	10.3.29-MariaDB-1:10.3.29+maria~bionic
+-- Server version	10.5.7-MariaDB-1:10.5.7+maria~focal
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,53 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `CaracteristicasProducto`
---
-
-DROP DATABASE publisher;
-CREATE DATABASE publisher;
-USE publisher;
-
---
--- Table structure for table `imagen_producto`
---
-
-DROP TABLE IF EXISTS `imagen_producto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `imagen_producto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_producto` int(11) DEFAULT NULL,
-  `nombre` varchar(256) DEFAULT NULL,
-  `activa` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `imagen_producto`
---
-
-LOCK TABLES `imagen_producto` WRITE;
-/*!40000 ALTER TABLE `imagen_producto` DISABLE KEYS */;
-INSERT INTO `imagen_producto` VALUES 
-(1,1,'D-1.jpg',1),
-(2,1,'D-2.jpg',1),
-(3,1,'D-3.jpg',1),
-(4,1,'D-4.jpg',1),
-(5,1,'D-5.jpg',1),
-(6,2,'02-01.jpg',1),
-(7,2,'02-02.jpg',1),
-(8,3,'03-01.jpg',1),
-(9,3,'04-01.jpg',1),
-(10,3,'03-02.jpg',1),
-(11,4,'04-02.jpg',1),
-(12,4,'04-03.jpg',1);
-/*!40000 ALTER TABLE `imagen_producto` ENABLE KEYS */;
-UNLOCK TABLES;
-
 
 --
 -- Table structure for table `caracteristica_producto`
@@ -84,22 +37,35 @@ CREATE TABLE `caracteristica_producto` (
 
 LOCK TABLES `caracteristica_producto` WRITE;
 /*!40000 ALTER TABLE `caracteristica_producto` DISABLE KEYS */;
-INSERT INTO `caracteristica_producto` VALUES 
-(1,1,'Su resolucion XYZW',1),
-(2,1,'Manejalo con la voz',1),
-(3,1,'Conecta tus dispositivos',1),
-(4,1,'Dimensiones perfects',1),
-(5,1,'Entretenimiento sin fin',1),
-(6,2,'Futurista',1),
-(7,2,'Rebajada',1),
-(8,3,'Rematada',1),
-(9,3,'Dream shop',1),
-(10,3,'Excelente',1),
-(11,4,'Inmejorable',1),
-(12,4,'Perfecta',1);
+INSERT INTO `caracteristica_producto` VALUES (1,1,'Su resolucion XYZW123',1),(2,1,'Manejalo con la voz',1),(3,1,'Conecta tus dispositivos',1),(4,1,'Dimensiones perfects',1),(5,1,'Entretenimiento sin fin',1),(6,2,'Futurista',1),(7,2,'Rebajada',1),(8,3,'Rematada',1),(9,3,'Dream shop',1),(10,3,'Excelente',1),(11,4,'Inmejorable',1),(12,4,'Perfecta',1);
 /*!40000 ALTER TABLE `caracteristica_producto` ENABLE KEYS */;
 UNLOCK TABLES;
 
+--
+-- Table structure for table `imagen_producto`
+--
+
+DROP TABLE IF EXISTS `imagen_producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `imagen_producto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_producto` int(11) DEFAULT NULL,
+  `nombre` varchar(256) DEFAULT NULL,
+  `activa` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `imagen_producto`
+--
+
+LOCK TABLES `imagen_producto` WRITE;
+/*!40000 ALTER TABLE `imagen_producto` DISABLE KEYS */;
+INSERT INTO `imagen_producto` VALUES (1,1,'D-1.jpg',1),(2,1,'D-2.jpg',1),(3,1,'D-3.jpg',1),(4,1,'D-4.jpg',1),(5,1,'D-5.jpg',1),(6,2,'02-01.jpg',1),(7,2,'02-02.jpg',1),(8,3,'03-01.jpg',1),(9,3,'03-02.jpg',1),(10,3,'03-03.jpg',1),(11,4,'04-01.jpg',1),(12,4,'04-02.jpg',1),(13,4,'04-03.jpg',1),(14,4,'04-04.jpg',1),(15,4,'04-05.jpg',1),(16,4,'04-06.jpg',1),(17,4,'04-07.jpg',1);
+/*!40000 ALTER TABLE `imagen_producto` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `producto`
@@ -138,11 +104,7 @@ CREATE TABLE `producto` (
 
 LOCK TABLES `producto` WRITE;
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-INSERT INTO `producto` VALUES 
-(1,'Pantalla JVC SI32H LCD HD 32 110V/240V',4,23,63,2916,917,57,'Cuernavaca Morelos',500,32,24,'Glogi',1000,3,'Martes', 'descipcion 01', 1, 'D-1.jpg'),
-(2,'Sony PlayStation 5 825GB color blanco y negro',5,500,500,13594,1359,180,'Jiutepec Morelos',7000,1,89,'Sony enterteiment',900,10,'Domingo', 'descipcion 02', 1, '02-01.jpg'),
-(3,'Metabo Ge 950 G Plus Mototool/rectificador Recto 950w 110v',4,2,4,7978,664,180,'Tlaltizapan Morelos',5,10,1,'Metabo',40,12,'Jueves', '', 1, '03-01.jpg'),
-(4,'Interfaz de audio Focusrite Scarlett Solo 220V roja 3.ª gen',5,68,500,2756,275,250,'Tepoztlan Morelos',100,5,1,'Milan Inter',100,10,'Miercoles', 'descipcion 03', 1, '04-02.jpg');
+INSERT INTO `producto` VALUES (1,'Pantalla JVC SI32H LCD HD 32 110V/240V',2.8,23,63,2916,917,57,'Cuernavaca Morelos',500,32,24,'Glogi',1000,3,'Martes','descipcion 01',1,'D-1.jpg'),(2,'Sony PlayStation 5 825GB color blanco y negro',5.0,500,500,13594,1359,180,'Jiutepec Morelos',7000,1,89,'Sony enterteiment',900,10,'Domingo','descipcion 02',1,'02-01.jpg'),(3,'Metabo Ge 950 G Plus Mototool/rectificador Recto 950w 110v',4.0,2,4,7978,664,180,'Tlaltizapan Morelos',5,10,1,'Metabo',40,12,'Jueves','',1,'03-01.jpg'),(4,'Interfaz de audio Focusrite Scarlett Solo 220V roja 3.ª gen',5.0,68,500,2756,275,250,'Tepoztlan Morelos',100,5,1,'Milan Inter',100,10,'Miercoles','descipcion 03',1,'04-02.jpg');
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -155,9 +117,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-14 13:12:13
-
-
-
-
-
+-- Dump completed on 2023-03-16  7:09:18
