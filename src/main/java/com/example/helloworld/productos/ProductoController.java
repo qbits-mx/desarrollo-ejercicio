ec2a85e38922d5677ca394e77ad7bf642befca4c
arellano-gustavo <arellano.gustavo@gmail.com>
2023-03-16 02:02:17 -0600

    Adding the new database schema

--- src/main/java/com/example/helloworld/productos/ProductoController.java
+++ src/main/java/com/example/helloworld/productos/ProductoController.java
@@ -1,7 +1,5 @@
 package com.example.helloworld.productos;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
