b13a7127ff975b23ab592b65628be4551d291fc7
guschiha <guschiha@gmail.com>
2023-03-22 10:39:41 -0600

    Digestion

--- src/main/java/com/example/helloworld/productos/ProductoController.java
+++ src/main/java/com/example/helloworld/productos/ProductoController.java
@@ -1,10 +1,16 @@
 package com.example.helloworld.productos;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.helloworld.productos.model.ResultadoConsulta;
+import com.example.helloworld.utileria.Digestion;
 
 @RestController
 @RequestMapping(value="/hola")
@@ -21,4 +27,46 @@ public class ProductoController {
     public ResultadoConsulta listaTotal(int id) {
         return this.productoService.getBigResult(id);
     }
+    @GetMapping(
+            value="/login", 
+            produces = "application/json; charset=utf-8")
+    public String fakeLogin(
+            @RequestParam String user, 
+            @RequestParam String fecha,
+            @RequestParam String password) {
+        SimpleDateFormat sdf=new  SimpleDateFormat ("dd/MM/yyyy");   
+        SimpleDateFormat sf=new  SimpleDateFormat ("hh:mm:ss");   
+
+        Date d = new Date();
+        String sFecha= sdf.format(d);
+        String shora= sf.format(d);
+
+
+        if(user.equals("gus") && password.equals("tavo")&&  sFecha.equals(fecha)) {
+            String cadenota = "{'user':'gus', 'exp':'22-03-2023', 'rol':'admin', 'pago':true  'hora':"+ shora +"  }";
+            return cadenota + "_" + Digestion.generateMd5(cadenota);
+
+        }
+        return "{'error':'bad pasword or user'}";
+    }
+    @GetMapping(
+            value="/cambia", 
+            produces = "application/json; charset=utf-8")
+    public String fakeCambiaPassword(
+            @RequestHeader String tokenDado,
+            @RequestParam String nuevoPassword) {
+        if(revisa(tokenDado)) {
+            return "{'exito':'password cambiado'}";
+        }
+        return "{'error':'bad token'}";
+    }
+
+    private boolean revisa(String tokenDado) {
+        String[] arreglo = tokenDado.split("_"); 
+        String cadenaOriginal = arreglo[0];
+        String hash = arreglo[1];
+        String digestion = Digestion.generateMd5(cadenaOriginal);
+        return (digestion.equals(hash));
+    }
+
 }
---
+++ src/main/java/com/example/helloworld/utileria/Digestion.java
@@ -0,0 +1,33 @@
+package com.example.helloworld.utileria;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Digestion {
+        public static String generateMd5(String input) {
+            String palablraSecreta="Arellano1967";
+            byte[] arreglo;
+            try {
+                arreglo = generateSaltedMd5(input, palablraSecreta);
+            } catch (NoSuchAlgorithmException e) {
+                return "ERROR";
+            }
+            return toHexString(arreglo);
+        }
+        private static byte[] generateSaltedMd5(String source, String salt) throws NoSuchAlgorithmException {
+            String input = source + salt;
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return md.digest(input.getBytes(StandardCharsets.UTF_8));
+        }
+        private static String toHexString(byte[] hash) {
+            BigInteger number = new BigInteger(1, hash);
+            String hexa = number.toString(16);
+            StringBuilder hexString = new StringBuilder(hexa);
+            while (hexString.length() < 32) {
+                hexString.insert(0, '0');
+            }
+            return hexString.toString();
+        }
+    }
