a28a7d175f2698cea7414be807fcbcfec268e522
arellano-gustavo <arellano.gustavo@gmail.com>
2023-03-22 13:10:27 -0600

    nuevo código de jwt process

--- src/main/java/com/example/helloworld/productos/ProductoController.java
+++ src/main/java/com/example/helloworld/productos/ProductoController.java
@@ -2,6 +2,7 @@ package com.example.helloworld.productos;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.Date;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -19,9 +20,11 @@ import com.example.helloworld.utilerias.Digestion;
 @RequestMapping(value="/hola")
 public class ProductoController {
     private ProductoService productoService;
+    private LoginService loginService;
     
-    public ProductoController(ProductoService productoService) {
+    public ProductoController(ProductoService productoService, LoginService loginService) {
         this.productoService = productoService;
+        this.loginService = loginService;
     }
     
     @GetMapping(
@@ -36,51 +39,27 @@ public class ProductoController {
     public String fakeLogin(
             @RequestParam String user, 
             @RequestParam String password) {
-        if(user.equals("gus") && password.equals("tavo")) {
-            String cadenota = "{'user':'gus', 'exp':'2020-02-20 14:01:27', 'rol':'admin', 'pago':true}";
-            return cadenota + "_" + Digestion.generateMd5(cadenota);
-        }
-        return "{'error':'bad pasword or user'}";
+        return loginService.login(user, password);
     }
+    
     @GetMapping(
             value="/cambia", 
             produces = "application/json; charset=utf-8")
     public String fakeCambiaPassword(
             @RequestHeader String tokenDado,
             @RequestParam String nuevoPassword) {
-        if(revisa(tokenDado)) {
-            return "{'exito':'password cambiado'}";
-        }
-        return "{'error':'bad token'}";
+        return loginService.cambia(tokenDado, nuevoPassword);
     }
 
-    private boolean revisa(String tokenDado) {
-        // también si no me llega algo de la forma xxxxxx_yyyyy return false;
-        if (!tokenDado.contains("_")) {
-            return false;
-        }
-        
-        String[] arreglo = tokenDado.split("_");
-        String cadenaOriginal = arreglo[0];
-        String hash = arreglo[1];
-        String digestion = Digestion.generateMd5(cadenaOriginal);
 
-        // if d>fecha_en_token => return false;
-        Date fechaActual = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String fechaEnToken = "2023-03-22 14:01:29"; // Cambia esto por la fecha que obtienes del token
-
-        try {
-            Date fechaToken = sdf.parse(fechaEnToken);
-            if (fechaActual.compareTo(fechaToken) > 0) {
-                return false;
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        return (digestion.equals(hash));
-    }
-
+    
+//    public static void main(String...argv) {
+//        String q1 = "gustavo@_hola 123:456  .;-ñ";
+//        String q2="{'user':'gus_plus', 'exp':'2023-03-22 14:17:01'}";
+//        String enc = base64encode(q2);
+//        System.out.println(enc);
+//        
+//        String dec = base64decode(enc);
+//        System.out.println(dec);
+//    }
 }
