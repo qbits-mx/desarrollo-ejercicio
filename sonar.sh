b5639d81de21dc238df6131a5556fc9e573ca15d
guschiha <guschiha@gmail.com>
2023-03-31 20:26:13 -0600

    comen

--- sonar.sh
+++ sonar.sh
@@ -2,4 +2,4 @@ mvn clean verify sonar:sonar \
     -Dsonar.login=347bc3b1b1b309dd491aa517ece54b3168c83427 \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=prueba-gus \
-    -Dsonar.projectKey=kix77-mx
+    -Dsonar.projectKey=prueba-gus
