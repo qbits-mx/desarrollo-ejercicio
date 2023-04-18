698e3ca60d081a04f12cfe702d7377280ac1243d
juan carlos ramos <jcramosc@desarrollo-utrasist.com.mx>
2023-04-18 09:59:20 -0600

    hola

---
+++ sonar.sh
@@ -0,0 +1,5 @@
+mvn clean verify sonar:sonar \
+    -Dsonar.login=59dfbac06cffcc524e5da7fd7ff148bb8e9d53c0 \
+    -Dsonar.host.url=https://sonarcloud.io/ \
+    -Dsonar.organization=jc-mx \
+    -Dsonar.projectKey=jc-mx_desarrollo_ejercicio
